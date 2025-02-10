5959167baf54ffd24074cf44f1ef0c34dfcf24e9
andrino <earlbrandx@gmail.com>
2025-02-10 14:51:43 +0800

    contact not functioning yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,51 @@
-
-import React, { useEffect } from 'react';
-import Home from './pages/Home';
-import About from './pages/About';
-import Works from './pages/Works';
-import Contact from './pages/Contact';
-import Navbar from './pages/NavBar';
+import React, { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Works from "./pages/Works";
+import Contact from "./pages/Contact";
+import Navbar from "./pages/NavBar";
 import Resume from "./pages/Resume";
+import Loader from "./pages/Loader";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import "./css/App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
 
   useEffect(() => {
     document.title = "Average Portfolio";
+
+    setTimeout(() => {
+      setFadeOut(true); 
+      setTimeout(() => {
+        setLoading(false); 
+      }, 500); 
+    }, 2000);
   }, []);
 
   return (
     <div className="App">
-      <Router> 
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          <Routes> 
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/works" element={<Works />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/resume" element={<Resume />} />
-          </Routes>
+      {loading ? (
+        <div className={`loader-container ${fadeOut ? "fade-out" : ""}`}>
+          <Loader />
         </div>
-      </div>
-    </Router>
+      ) : (
+        <Router>
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/works" element={<Works />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/resume" element={<Resume />} />
+            </Routes>
+          </div>
+        </Router>
+      )}
     </div>
-    
-);
-};
-export default App
+  );
+}
+
+export default App;
